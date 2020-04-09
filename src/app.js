902b0accbbd13a6416ecf9a85e7d647c783415c7
Victor Radael <victorradael@hotmail.com>
2020-04-09 14:17:44 -0300

    challenge solved and tested, passing all tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRegister = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRegister);
+
+  return response.send(newRegister);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (repository) {
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+  } else {
+    return response.status(400).send({ error: "Repositorio nao encontrado" });
+  }
+
+  return response.send(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if(repositoryIndex && repositoryIndex !== -1){
+    repositories.splice(repositoryIndex, 1);
+  } else {
+    return response.status(400).send({error: "Repositorio nao encontrado"});
+  }
+  
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params;
+  
+    const repository = repositories.find((repository) => repository.id === id);
+  
+    if (repository) {
+      repository.likes++;
+      
+    } else {
+      return response.status(400).send({ error: "Repositorio nao encontrado" });
+    }
+
+    return response.send(repository)
 });
 
 module.exports = app;
